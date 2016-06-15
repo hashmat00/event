36d6612ed9818722094c4c129fa19d72d7d1e760
hashmat MH <hashmat00@aol.com>
2016-06-15 02:09:21 +0000

    initial update

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -9,7 +9,7 @@ if Rails.env.production?
         :aws_access_key_id => ENV['S3_ACCESS_KEY'],
         
         :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-        :region                 => 'us-east-1'
+        :region                 => 'us-west-1'
         
         }
     
