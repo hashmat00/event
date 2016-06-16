3ef1c0a33268a4a46f215e0a31de8ad098107077
hashmat MH <hashmat00@aol.com>
2016-06-16 09:19:50 +0000

    fixed organizer listing on homepage where it shows duplicate users

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
     
     def home
-        @users = User.paginate(page: params[:page], per_page: 4)
+        @users = User.paginate(page: params[:page], per_page: 3)
         @events = Event.paginate(page: params[:page], per_page: 3)
         redirect_to events_path if logged_in?
         
