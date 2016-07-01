4c1662c35ba89e4c5d167c61b2ff1067dc5a7cd1
ROR Future <rorfuture@gmail.com>
2016-07-01 11:32:32 +0530

    commit

--- app/admin/event_category.rb
+++ app/admin/event_category.rb
@@ -1,22 +1,12 @@
-ActiveAdmin.register User do
-  config.filters = false
+ActiveAdmin.register EventCategory do
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :usersname, :email, :password, :password_confirmation
+permit_params :category_id, :event_id
 #
 # or
 #
-  form do |f|
-    f.inputs "User Details" do
-      f.input :username
-      f.input :email
-      f.input :password
-      f.input :password_confirmation
-    end
-    f.actions
-  end
-
 # permit_params do
 #   permitted = [:permitted, :attributes]
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
@@ -25,3 +15,4 @@ permit_params :usersname, :email, :password, :password_confirmation
 
 
 end
+
--- app/admin/order.rb
+++ app/admin/order.rb
@@ -1,22 +1,12 @@
-ActiveAdmin.register User do
-  config.filters = false
+ActiveAdmin.register Order do
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :usersname, :email, :password, :password_confirmation
+permit_params :subtotal, :tax, :shipping, :total, :order_status_id, :notification_params, :status, :transaction_id, :purchased_at, :user_id
 #
 # or
 #
-  form do |f|
-    f.inputs "User Details" do
-      f.input :username
-      f.input :email
-      f.input :password
-      f.input :password_confirmation
-    end
-    f.actions
-  end
-
 # permit_params do
 #   permitted = [:permitted, :attributes]
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
@@ -25,3 +15,5 @@ permit_params :usersname, :email, :password, :password_confirmation
 
 
 end
+
+
