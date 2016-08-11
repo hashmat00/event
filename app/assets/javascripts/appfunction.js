36967a2c5d5aa03bdf30ea32d199ea132e787d69
development <developmentgit@gmail.com>
2016-08-11 11:25:36 +0530

    commit

--- app/assets/javascripts/appfunction.js
+++ app/assets/javascripts/appfunction.js
@@ -21,4 +21,78 @@ $(document).ready(function() {
         });
 });
 
+$(document).ready(function(){
+  $('#new_event :radio').change(function(){
+    var nxtTxtFld = $(this).parent().parent().find('input[type=text]');
+    if ( nxtTxtFld.length ) {
+        nxtTxtFld.show();
+        $("input[type=text]:not(#"+nxtTxtFld.attr('id')+")").hide();
+    }
+    else
+    {
+      filterFormSubmit();
+    }
+
+  });
+  $("input[type=text]").change(function(){
+    filterFormSubmit();
+  }
+  );
+  $('#new_event :checkbox').change(function(){
+    filterFormSubmit();
+  });
+
+  function filterFormSubmit()
+  {
+    $('.manualInput').hide();
+    $('#new_event').submit(); 
+  }
+  
+  $(".price-input").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 100000,
+    from: 100,
+    to: 100,
+    keyboard: true,
+    step: 10,
+    grid: true,
+    force_edges: true,
+    onFinish: function (data) {
+        console.log("onFinish");
+        $('#new_event').submit();
+    }
+  });
+});
+
+
+
+// $(document).ready(function() {
+//   $('#new_event, .edit_event').formValidation({
+//     framework: 'bootstrap',
+//     excluded: [':disabled'],
+//    	icon: {
+//       valid: 'glyphicon glyphicon-ok',
+//       invalid: 'glyphicon glyphicon-remove',
+//       validating: 'glyphicon glyphicon-refresh'
+//     },
+//     fields: {
+//       'event[name]': {
+//         row: '.col-xs-4',
+//         validators: {
+//           notEmpty: {
+//             message: 'The event name is required'
+//           },
+//           stringLength: {
+//           	min: 3,
+//           	max: 100,
+//           	message: 'The event name must be more than 3 and less than 100 characters long'
+//           }
+//         }
+//       }
+//     }
+//   });
+// });
+
+
 
